322423456aff3b285fd90f9c87a4a624abf5c9c7
Jordan <jcedeno870@gmail.com>
2023-02-05 16:54:33 -0600

    Navigation Pages - WIP

--- App.js
+++ App.js
@@ -1,23 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, React } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-export default function App() {
-  let x = 1;
-  console.log("App executed");
-
+const Home = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Home</Text>
     </View>
+  );
+};
+
+const Pantry = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Pantry</Text>
+    </View>
+  );
+};
+
+const Cookbook = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Cookbook</Text>
+    </View>
+  );
+};
+
+const Tab = createBottomTabNavigator();
+
+const Tabs = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Pantry" component={Pantry} />
+      <Tab.Screen name="Cookbook" component={Cookbook} />
+    </Tab.Navigator>
+  );
+};
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tabs />
+    </NavigationContainer>
+    // <View style={styles.container}>
+    //   <Text>Open up App.js to start working on your app!</Text>
+    //   <StatusBar style="auto" />
+    // </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  view: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
