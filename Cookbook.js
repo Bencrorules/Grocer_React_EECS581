1a3f4ffe31f33af5ecd314882bbb1f30156c53e1
Jordan <jcedeno870@gmail.com>
2023-02-06 13:36:56 -0600

    small refactoring

--- Cookbook.js
+++ Cookbook.js
@@ -6,25 +6,28 @@ import {
   TouchableOpacity,
 } from "react-native";
 
+const DATA = [
+  { id: "1", key: "Recipe1" },
+  { id: "2", key: "Recipe2" },
+  { id: "3", key: "Recipe3" },
+  { id: "4", key: "Recipe4" },
+  { id: "5", key: "Recipe5" },
+  { id: "6", key: "Recipe6" },
+  { id: "7", key: "Recipe7" },
+  { id: "8", key: "Recipe8" },
+];
+
 const Cookbook = () => {
   return (
     <View style={styles.container}>
       <FlatList
-        data={[
-          { key: "Recipe" },
-          { key: "Recipe" },
-          { key: "Recipe" },
-          { key: "Recipe" },
-          { key: "Recipe" },
-          { key: "Recipe" },
-          { key: "Recipe" },
-          { key: "Recipe" },
-        ]}
+        data={DATA}
         renderItem={({ item }) => (
           <TouchableOpacity>
             <Text style={styles.item}>{item.key}</Text>
           </TouchableOpacity>
         )}
+        keyExtractor={(item) => item.id}
       />
     </View>
   );
@@ -39,6 +42,7 @@ const styles = StyleSheet.create({
     padding: 10,
     fontSize: 18,
     height: 44,
+    color: "blue",
   },
 });
 
